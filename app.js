4d06d0e1da8d5adf2272bfa5474c472df31c58d0
Adrielli <driih.abs@gmail.com>
2025-08-21 15:37:37 -0400

    Função adicionarAmigo feita

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+
+function adicionarAmigo(){
+    let amigosInseridos = document.querySelector('input').value;
+    if (amigosInseridos == ""){
+        alert('Digite um nome, por favor!');
+    } else {
+        amigos.push(amigosInseridos);
+        console.log(amigos);
+        limparCampo();
+        listaDeAmigos(amigos);
+    }
+}
+
+
