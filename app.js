12f33c89616bd01f73528d9314ab912892859411
Adrielli <driih.abs@gmail.com>
2025-08-21 15:47:30 -0400

    Fim do projeto

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 let amigos = [];
 
-
 function adicionarAmigo(){
     let amigosInseridos = document.querySelector('input').value;
     if (amigosInseridos == ""){
@@ -17,5 +16,29 @@ function limparCampo(){
     amigosInseridos = document.querySelector('input');
     amigosInseridos.value = '';
 }
+
+function listaDeAmigos(amigos){
+    let lista = document.querySelector('ul'); //Seleciona onde a lista vai ficar
+    lista.innerHTML = ''; //Limpa a lista quando for atualizado
+
+    
+    amigos.forEach(function(amigos) {
+        let novoAmigoInserido = document.createElement('li');
+        novoAmigoInserido.innerHTML = amigos;
+        lista.appendChild(novoAmigoInserido);
+    });    
+}
+
+function sortearAmigo(){
+    if(amigos == ''){
+        alert('Você não possui amigos para sortear!!');
+    } else {
+        let indiceSorteio = Math.floor(Math.random() * amigos.length);
+        let amigoSorteado = amigos[indiceSorteio];
+        let resultado = document.getElementById('resultado');
+        resultado.innerHTML= amigoSorteado;
+    }
+}
+
 
 
